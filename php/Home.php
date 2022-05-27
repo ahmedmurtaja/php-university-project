1b21b2214e01263308ad4b3d8ec337f02ce42a8d
ahmedmurtaja <ahmed.izz.murtaja@gmail.com>
2022-05-27 11:48:07 +0300

    first page Done

--- php/Home.php
+++ php/Home.php
@@ -1,3 +1,7 @@
+<?php
+include_once('../includes/DBconnection.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,20 +13,20 @@
     <!-- <link rel="stylesheet" href="../css/secound.css"> -->
 </head>
 <body>
-    <div id="left"></div>
+    <!-- <div id="left"></div>
     <div id="right"></div>
     <div id="top"></div>
-    <div id="bottom"></div>
+    <div id="bottom"></div> -->
     <header>
         <nav>
              <div class="container">
                  <div class="contain">
                    <ul class="nav-links">
                        <li><a href="#" class="active">Personal Information</a></li>
-                       <li><a href="ViewCourses.html">Courses Information</a></li>
-                       <li><a href="ViewExperience.html">Experience Information</a></li>
-                       <li><a href="AddCourse.html">Add Course</a></li>
-                       <li><a href="AddExperience.html">Add Experience</a></li>
+                       <li><a href="ViewCourses.php">Courses Information</a></li>
+                       <li><a href="ViewExperience.php">Experience Information</a></li>
+                       <li><a href="AddCourse.php">Add Course</a></li>
+                       <li><a href="AddExperience.php">Add Experience</a></li>
                    </ul>
                    <div class="logo">
                        <img src="../images/Azhar_WHITE_LOGO.png" alt="Logo" width="70" height="70">
@@ -38,34 +42,54 @@
                 
                 <div class="personal-info">
                     <table cellspacing="30">
-                        <tr>
-                            <td>Full Name: </td>
-                            <td><b>Ahmed Izz-Al-Din Murtaja</b></td>
-                        </tr>
-                        <tr>
-                            <td>Gender: </td>
-                            <td><b>Male</b></td>
-                        </tr>
-                        <tr>
-                            <td>Birth Date: </td>
-                            <td><b>9<sup>th</sup>, <abbr title="June">Jun</abbr> 2001</b></td>
-                        </tr>
-                        <tr>
-                            <td>Notionality: </td>
-                            <td><b>Palestinian</b></td>
-                        </tr>
-                        <tr>
-                            <td>Place of Birth </td>
-                            <td><b>Gaza</b></td>
-                        </tr>
-                        <tr>
-                            <td>Job Title: </td>
-                            <td><b>Software Engineering</b></td>
-                        </tr>
-                        <tr>
-                            <td>Year of experience: </td>
-                            <td><b>1 year</b></td>
-                        </tr>
+                      <?php
+
+						$query = "SELECT * from users";
+						$result = mysqli_query($connection, $query);
+
+						if (mysqli_num_rows($result) > 0) {
+							while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<tr>'
+                            .'<td>'.'Full Name:'.'</td>'
+                            .'<td>'.'<b>'.$row['name'].'</b>'.'</td>'
+                        .'</tr>'.
+                        
+                        '<tr>'
+                            .'<td>'.'Gender:'.'</td>'
+                            .'<td>'.'<b>'.$row['gender'].'</b>'.'</td>'
+                        .'</tr>'.
+
+                        '<tr>'
+                            .'<td>'.'Birth Date:'.'</td>'
+                            .'<td>'.'<b>'.$row['birthdate'].'</b>'.'</td>'
+                        .'</tr>'.
+
+                        '<tr>'
+                            .'<td>'.'Nationality:'.'</td>'
+                            .'<td>'.'<b>'.$row['nationality'].'</b>'.'</td>'
+                        .'</tr>'.
+
+                        '<tr>'
+                            .'<td>'.'Place Of Birth:'.'</td>'
+                            .'<td>'.'<b>'.$row['origin'].'</b>'.'</td>'
+                        .'</tr>'.
+
+                        '<tr>'
+                            .'<td>'.'Job Title:'.'</td>'
+                            .'<td>'.'<b>'.$row['job'].'</b>'.'</td>'
+                        .'</tr>'.
+                        
+                        '<tr>'
+                            .'<td>'.'Years Of Experience:'.'</td>'
+                            .'<td>'.'<b>'.$row['exp'].'  years'.'</b>'.'</td>'
+                        .'</tr>';
+                        break;
+                            }
+                        }
+                        mysqli_close($connection);
+                        
+                          ?>  
+                        
                     </table>
                     <div class="image">
                         <img src="../images/Screenshot 2021-12-24 202108.png" alt="" >
