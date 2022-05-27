4918123bbd23f5a7e466fbb5a780b9b1fe8bfcd8
ahmedmurtaja <ahmed.izz.murtaja@gmail.com>
2022-05-27 11:18:27 +0300

    rename files

---
+++ php/Home.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Website</title>
+    <link rel="stylesheet" href="../css/MyStyle.css"> 
+    <!-- <link rel="stylesheet" href="../css/secound.css"> -->
+</head>
+<body>
+    <div id="left"></div>
+    <div id="right"></div>
+    <div id="top"></div>
+    <div id="bottom"></div>
+    <header>
+        <nav>
+             <div class="container">
+                 <div class="contain">
+                   <ul class="nav-links">
+                       <li><a href="#" class="active">Personal Information</a></li>
+                       <li><a href="ViewCourses.html">Courses Information</a></li>
+                       <li><a href="ViewExperience.html">Experience Information</a></li>
+                       <li><a href="AddCourse.html">Add Course</a></li>
+                       <li><a href="AddExperience.html">Add Experience</a></li>
+                   </ul>
+                   <div class="logo">
+                       <img src="../images/Azhar_WHITE_LOGO.png" alt="Logo" width="70" height="70">
+                   </div> 
+                 </div>
+             </div>
+       </nav>
+   </header>
+   <main>
+       <div class="container">
+            <section class="infos">
+                <h1 class="infos-title">Personal Information</h1>
+                
+                <div class="personal-info">
+                    <table cellspacing="30">
+                        <tr>
+                            <td>Full Name: </td>
+                            <td><b>Ahmed Izz-Al-Din Murtaja</b></td>
+                        </tr>
+                        <tr>
+                            <td>Gender: </td>
+                            <td><b>Male</b></td>
+                        </tr>
+                        <tr>
+                            <td>Birth Date: </td>
+                            <td><b>9<sup>th</sup>, <abbr title="June">Jun</abbr> 2001</b></td>
+                        </tr>
+                        <tr>
+                            <td>Notionality: </td>
+                            <td><b>Palestinian</b></td>
+                        </tr>
+                        <tr>
+                            <td>Place of Birth </td>
+                            <td><b>Gaza</b></td>
+                        </tr>
+                        <tr>
+                            <td>Job Title: </td>
+                            <td><b>Software Engineering</b></td>
+                        </tr>
+                        <tr>
+                            <td>Year of experience: </td>
+                            <td><b>1 year</b></td>
+                        </tr>
+                    </table>
+                    <div class="image">
+                        <img src="../images/Screenshot 2021-12-24 202108.png" alt="" >
+                    </div>          
+                </div>
+            </section> 
+       </div>
+   </main>   
+</body>
+</html>
